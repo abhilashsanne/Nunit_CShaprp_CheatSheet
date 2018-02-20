f36c915585a128a71e0260ad40cdfa0120449dfc
Abhilash Sanne <sanne.abhi@gmail.com>
2018-02-20 16:34:17 +0530

    Test Checkin
    Test Checkin

---
+++ Nunit_CShaprp_CheatSheet/NUnitTestFixture.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Nunit_CShaprp_CheatSheet
+{
+    [TestFixture]
+    public class NUnitTestFixture
+    {
+        public NUnitTestFixture()
+        {
+        }
+
+        #region Setup Methods
+        [TestFixtureSetUp]
+        public void TestClassFixture()
+        {
+        }
+        [SetUp]
+        public void TestSetup()
+        {
+        }
+        #endregion
+
+        #region Individual Tests
+        
+        #endregion
+
+        #region Test Suites
+
+        #endregion
+
+        #region DataDriven Tests
+        [Test]
+        public void Test()
+        {
+
+        }
+        #endregion
+       
+      
+       
+
+        #region TearDown Methods
+        [TearDown]
+        public void TestTearDown()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TestClassTearDown()
+        {
+
+          
+        }
+        #endregion
+       
+    }
+}
+
