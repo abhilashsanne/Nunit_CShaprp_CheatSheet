888a6a339f2964217dd007b63321340027c07c92
Abhilash Sanne <sanne.abhi@gmail.com>
2018-02-23 20:39:22 +0530

    Added CustomPropertyTests along with Reading test properties

--- Nunit_CShaprp_CheatSheet/AssertionsAndConstraints.cs
+++ Nunit_CShaprp_CheatSheet/AssertionsAndConstraints.cs
@@ -2,9 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nunit_CShaprp_CheatSheet
 {
@@ -36,7 +33,7 @@ namespace Nunit_CShaprp_CheatSheet
 
             Assert.IsEmpty(new List<object>());
             Assert.IsEmpty(new List<object>());
-            Assert.IsNotEmpty(new List<object> { 1 });
+            Assert.IsNotEmpty(new List<object> {1});
             Assert.IsNotEmpty("MyTestString");
 
             #endregion
@@ -110,7 +107,7 @@ namespace Nunit_CShaprp_CheatSheet
             StringAssert.AreEqualIgnoringCase("DOG", "dog");
             StringAssert.IsMatch("[10-29]", "15");
 
-            Assert.Contains(string.Empty, new List<object> { string.Empty },
+            Assert.Contains(string.Empty, new List<object> {string.Empty},
                 "Assert.Contains is used to test whether an object is contained in a collection.");
 
             #endregion
@@ -125,25 +122,25 @@ namespace Nunit_CShaprp_CheatSheet
             //AreEquivalent tests whether the collection contents are equal, but without regard to order.
             //In both cases, elements are compared using NUnit's default equality comparison.
 
-            CollectionAssert.AllItemsAreInstancesOfType(new List<decimal> { 0m }, typeof(decimal));
-            CollectionAssert.AllItemsAreNotNull(new List<decimal> { 0m });
-            CollectionAssert.AllItemsAreUnique(new List<decimal> { 0m, 1m });
-            CollectionAssert.AreEqual(new List<decimal> { 0m }, new List<decimal> { 0m });
-            CollectionAssert.AreEquivalent(new List<decimal> { 0m, 1m },
-                new List<decimal> { 1m, 0m }); // Same as AreEqual though order does not mater
-            CollectionAssert.AreNotEqual(new List<decimal> { 0m }, new List<decimal> { 1m });
-            CollectionAssert.AreNotEquivalent(new List<decimal> { 0m, 1m },
-                new List<decimal> { 1m, 2m }); // Same as AreNotEqual though order does not matter
-            CollectionAssert.Contains(new List<decimal> { 0m, 1m }, 1m);
-            CollectionAssert.DoesNotContain(new List<decimal> { 0m, 1m }, 2m);
-            CollectionAssert.IsSubsetOf(new List<decimal> { 1m },
-                new List<decimal> { 0m, 1m }); // {1} is considered a SubSet of {1,2}
-            CollectionAssert.IsNotSubsetOf(new List<decimal> { 1m, 2m }, new List<decimal> { 0m, 1m });
+            CollectionAssert.AllItemsAreInstancesOfType(new List<decimal> {0m}, typeof(decimal));
+            CollectionAssert.AllItemsAreNotNull(new List<decimal> {0m});
+            CollectionAssert.AllItemsAreUnique(new List<decimal> {0m, 1m});
+            CollectionAssert.AreEqual(new List<decimal> {0m}, new List<decimal> {0m});
+            CollectionAssert.AreEquivalent(new List<decimal> {0m, 1m},
+                new List<decimal> {1m, 0m}); // Same as AreEqual though order does not mater
+            CollectionAssert.AreNotEqual(new List<decimal> {0m}, new List<decimal> {1m});
+            CollectionAssert.AreNotEquivalent(new List<decimal> {0m, 1m},
+                new List<decimal> {1m, 2m}); // Same as AreNotEqual though order does not matter
+            CollectionAssert.Contains(new List<decimal> {0m, 1m}, 1m);
+            CollectionAssert.DoesNotContain(new List<decimal> {0m, 1m}, 2m);
+            CollectionAssert.IsSubsetOf(new List<decimal> {1m},
+                new List<decimal> {0m, 1m}); // {1} is considered a SubSet of {1,2}
+            CollectionAssert.IsNotSubsetOf(new List<decimal> {1m, 2m}, new List<decimal> {0m, 1m});
             CollectionAssert.IsEmpty(new List<decimal>());
-            CollectionAssert.IsNotEmpty(new List<decimal> { 1m });
-            CollectionAssert.IsOrdered(new List<decimal> { 1m, 2m, 3m });
-            CollectionAssert.IsOrdered(new List<string> { "a", "A", "b" }, StringComparer.CurrentCultureIgnoreCase);
-            CollectionAssert.IsOrdered(new List<int> { 3, 2, 1 },
+            CollectionAssert.IsNotEmpty(new List<decimal> {1m});
+            CollectionAssert.IsOrdered(new List<decimal> {1m, 2m, 3m});
+            CollectionAssert.IsOrdered(new List<string> {"a", "A", "b"}, StringComparer.CurrentCultureIgnoreCase);
+            CollectionAssert.IsOrdered(new List<int> {3, 2, 1},
                 "The list is ordered"); // Only supports ICompare and not ICompare<T> as of version 2.6
 
             #endregion
@@ -211,7 +208,7 @@ namespace Nunit_CShaprp_CheatSheet
             Assert.That("Foo!", Is.EqualTo("Foo!"));
             Assert.That("Foo!", Is.Not.EqualTo("FOO!"));
             Assert.That("Foo!", Is.EqualTo("FOO!").IgnoreCase);
-            Assert.That(new List<string> { "FOO!" }, Is.EqualTo(new List<string> { "Foo!" }).IgnoreCase);
+            Assert.That(new List<string> {"FOO!"}, Is.EqualTo(new List<string> {"Foo!"}).IgnoreCase);
 
             // Date, Time and TimeSpan equality
             Assert.That(DateTime.Today, Is.EqualTo(DateTime.Today));
@@ -225,10 +222,10 @@ namespace Nunit_CShaprp_CheatSheet
             Assert.That(DateTime.Now, Is.EqualTo(DateTime.Now).Within(1).Milliseconds);
 
             // Collection equality
-            Assert.That(new[] { 1.0, 2.0, 3.0 }, Is.EqualTo(new[] { 1, 2, 3 }));
-            Assert.That(new[] { 1.0, 2.0, 3.0 }, Is.Not.EqualTo(new[] { 1, 2, 3, 4 }));
-            Assert.That(new[] { 1.0, 2.0, 3.0, 4.0 },
-                Is.EqualTo(new[,] { { 1, 2 }, { 3, 4 } })
+            Assert.That(new[] {1.0, 2.0, 3.0}, Is.EqualTo(new[] {1, 2, 3}));
+            Assert.That(new[] {1.0, 2.0, 3.0}, Is.Not.EqualTo(new[] {1, 2, 3, 4}));
+            Assert.That(new[] {1.0, 2.0, 3.0, 4.0},
+                Is.EqualTo(new[,] {{1, 2}, {3, 4}})
                     .AsCollection); // Compare data and not collection (flattens a collection of collections)
 
             // Customer Comparer
@@ -245,7 +242,7 @@ namespace Nunit_CShaprp_CheatSheet
             Assert.That(double.NaN, Is.NaN);
             Assert.That(string.Empty, Is.Empty);
             Assert.That(new List<int>(), Is.Empty);
-            Assert.That(new List<int> { 1, 2, 3 }, Is.Unique);
+            Assert.That(new List<int> {1, 2, 3}, Is.Unique);
 
             // Comparision
             Assert.That(1, Is.LessThan(2));
@@ -283,16 +280,16 @@ namespace Nunit_CShaprp_CheatSheet
             Assert.That("Foo", Is.StringMatching("^[F]")); // Regular ecpression match
 
             // Collection Comparison
-            Assert.That(new List<int> { 1, 2, 3 }, Has.All.GreaterThan(0));
-            Assert.That(new List<int> { 1, 2, 3 }, Is.All.GreaterThan(0));
-            Assert.That(new List<int> { 1, 2, 3 }, Has.None.GreaterThan(4));
-            Assert.That(new List<int> { 1, 2, 3 }, Has.Some.GreaterThan(2));
-            Assert.That(new List<int> { 1, 2, 3 }, Has.Count.EqualTo(3));
-            Assert.That(new List<int> { 1, 2, 3 }, Is.Unique);
-            Assert.That(new List<int> { 1, 2, 3 }, Has.Member(1)); // Contains
-            Assert.That(new List<int> { 1, 2, 3 },
-                Is.EquivalentTo(new List<int> { 3, 2, 1 })); // Same data without carring about the order
-            Assert.That(new List<int> { 1, 2, }, Is.SubsetOf(new List<int> { 3, 2, 1 })); // All are cotained.
+            Assert.That(new List<int> {1, 2, 3}, Has.All.GreaterThan(0));
+            Assert.That(new List<int> {1, 2, 3}, Is.All.GreaterThan(0));
+            Assert.That(new List<int> {1, 2, 3}, Has.None.GreaterThan(4));
+            Assert.That(new List<int> {1, 2, 3}, Has.Some.GreaterThan(2));
+            Assert.That(new List<int> {1, 2, 3}, Has.Count.EqualTo(3));
+            Assert.That(new List<int> {1, 2, 3}, Is.Unique);
+            Assert.That(new List<int> {1, 2, 3}, Has.Member(1)); // Contains
+            Assert.That(new List<int> {1, 2, 3},
+                Is.EquivalentTo(new List<int> {3, 2, 1})); // Same data without carring about the order
+            Assert.That(new List<int> {1, 2,}, Is.SubsetOf(new List<int> {3, 2, 1})); // All are cotained.
 
             // Property Constraint
             Assert.That(new List<int>(), Has.Property("Count").EqualTo(0)); // Checks public property
@@ -322,9 +319,10 @@ namespace Nunit_CShaprp_CheatSheet
 
             Assert.That(() => new ArgumentException(), Throws.ArgumentException);
 
-            // Compound Constraints
+            #region Compound Constraints
+
             Assert.That(2, Is.Not.EqualTo(1));
-            Assert.That(new List<int> { 1, 2, 3 }, Is.All.GreaterThan(0));
+            Assert.That(new List<int> {1, 2, 3}, Is.All.GreaterThan(0));
 
             Assert.That(1, Is.GreaterThan(0).And.LessThan(2)); // .And amd & are the same
             Assert.That(1, Is.GreaterThan(0) & Is.LessThan(2));
@@ -332,15 +330,32 @@ namespace Nunit_CShaprp_CheatSheet
             Assert.That(1, Is.LessThan(10) | Is.GreaterThan(0)); // .Or and | are the same
             Assert.That(1, Is.LessThan(10).Or.GreaterThan(0));
 
-            // Delayed
-            Assert.That(() => true, Is.True.After(10)); // Passes after x time
-            Assert.That(() => true, Is.True.After(10, 10)); // Passes after x time and polling..
+            #endregion
 
-            // List Mapper
+
+            #region Delayed Assertions
+
+            Assert.That(() => true, Is.True.After(10)); // Passes after x time
+            Assert.That(() => true, Is.True.After(30, 10)); // Passes after x time and polling..
+
+            #endregion
+
+            #region List Mapper
+
+            string[] strings = {"a", "ab", "abc"};
+            int[] lengths = {1, 2, 3};
+
+            Assert.That(List.Map(strings).Property("Length"), Is.EqualTo(lengths));
+
+            Assert.That(new ListMapper(strings).Property("Length"),
+                Is.EqualTo(lengths));
+
             Assert.That(
-                List.Map(new List<string> { "1", "22", "333" })
+                List.Map(new List<string> {"1", "22", "333"})
                     .Property("Length"), // Allows all elememts in a list to have a condition provided for them
-                Is.EqualTo(new List<int> { 1, 2, 3 }));
+                Is.EqualTo(new List<int> {1, 2, 3}));
+
+            #endregion
         }
     }
 }
--- Nunit_CShaprp_CheatSheet/CustomPropertyTests.cs
+++ Nunit_CShaprp_CheatSheet/CustomPropertyTests.cs
@@ -78,5 +78,53 @@ namespace Nunit_CShaprp_CheatSheet
 
         #endregion
 
+        #region About TestContext, CurrentContext
+
+        //TestContext
+        //Each NUnit test runs in an execution context, which includes information about the environment as well as the test itself. 
+        //The TestContext class allows tests to access certain information about the execution context. 
+
+
+        //CurrentContext
+        //The context of the currently executing test may be accessed by use of the static property CurrentContext, 
+        //which gets a reference to the current TestContext. This context is created separately for each test before it begins execution.
+
+
+        //NOTE: context may not be changed - all properties are read-only.
+        [TearDown]
+        public void Demo_TestContext_CurrentContext()
+        {
+            var test = TestContext.CurrentContext.Test;
+            var testName = TestContext.CurrentContext.Test.Name;
+            var fullName = TestContext.CurrentContext.Test.FullName;
+
+            //Gets the full path of the directory containing the current test assembly.
+            var testDirectory = TestContext.CurrentContext.TestDirectory;
+
+            //Gets the full path of the directory to be used for output from this test run
+            //This is normally the directory that was current when execution of NUnit began
+            //but may be changed by use of the /work option of nunit-console
+            var workingDirectory = TestContext.CurrentContext.WorkDirectory;
+
+            //A TestStatus with possible values Inconclusive Skipped Passed Failed
+            //The Status value should be used in preference to State wherever possible, since the latter will not be available in future releases.
+            var testStatus = TestContext.CurrentContext.Result.Status;
+
+
+            //A test can be in these states: Inconclusive NotRunnable Skipped Ignored Success Failure Error Cancelled
+            //The result of the test may be accessed during setup or test execution, but it only has a useful value at in the TearDown method.
+            var testState = TestContext.CurrentContext.Result.State;
+
+            Console.WriteLine(test);
+            Console.WriteLine(testName);
+            Console.WriteLine(fullName);
+            Console.WriteLine(testDirectory);
+            Console.WriteLine(workingDirectory);
+            Console.WriteLine(testStatus);
+            Console.WriteLine(testState);
+        }
+
+        #endregion
+
     }
 }
