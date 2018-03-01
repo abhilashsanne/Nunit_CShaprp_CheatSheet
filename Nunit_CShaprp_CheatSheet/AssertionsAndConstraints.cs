9364656986ac13c08d505c7fb73b23edd5b8b229
Bhargava Kanamarlapudi <bhargava.kanamarlapudi@cdk.com>
2018-03-01 15:59:31 +0530

    added assertions

--- Nunit_CShaprp_CheatSheet/AssertionsAndConstraints.cs
+++ Nunit_CShaprp_CheatSheet/AssertionsAndConstraints.cs
@@ -19,19 +19,21 @@ namespace Nunit_CShaprp_CheatSheet
             #region Condition assertions
 
             Assert.True(true, "Assert.True and Assert.IsTrue test that the specified condition is true. ");
+            
+
             Assert.IsTrue(true);
 
             Assert.False(false, "Assert.False and Assert.IsFalse test that the specified condition is false.");
             Assert.IsFalse(false);
-
+            
             Assert.Null(null);
             Assert.IsNull(null, "Assert.Null and Assert.IsNull test that the specified object is null.");
             Assert.IsNotNull("10");
             Assert.NotNull("10", "");
 
-            Assert.IsNaN(Double.NaN, "ssert.IsNaN tests that the specified double value is NaN (Not a number).");
-
-            Assert.IsEmpty(new List<object>());
+            Assert.IsNaN(Double.NaN, "Assert.IsNaN tests that the specified double value is NaN (Not a number).");
+            
+            Assert.IsEmpty("");
             Assert.IsEmpty(new List<object>());
             Assert.IsNotEmpty(new List<object> {1});
             Assert.IsNotEmpty("MyTestString");
@@ -50,7 +52,11 @@ namespace Nunit_CShaprp_CheatSheet
             Assert.AreSame(string.Empty, string.Empty,
                 "Assert.AreNotSame tests that the two arguments do not reference the same object.");
             Assert.AreNotSame(new object(), new object());
-
+            
+            //both objects are refering to same object
+            object a = new object();
+            object b = a;
+            Assert.AreSame(a,b);
             #endregion
 
             #region Comparision
@@ -79,34 +85,24 @@ namespace Nunit_CShaprp_CheatSheet
             Assert.IsNotAssignableFrom<List<Type>>(string.Empty);
             Assert.IsAssignableFrom<List<decimal>>(new List<decimal>());
 
-            #endregion
-
-            #region Exceptions
-
-            //Using lambda functions with no parameters for ease of explanation/Implementation
-
-            Assert.Throws(typeof(ArgumentNullException), () => new ArgumentNullException());
-
-            Assert.Throws<ArgumentNullException>(() => new ArgumentNullException());
-
-            Assert.DoesNotThrow(() => { });
-
-            Assert.Catch(typeof(Exception), () => new ArgumentNullException());
-
-            // Similar as Throws but allows any derrived class of the thrown exception
-            Assert.Catch<Exception>(() => new ArgumentNullException());
-
-            #endregion
+            #endregion            
+          
 
             #region Strings
 
             //The StringAssert class provides a number of methods that are useful when examining string values
-            StringAssert.Contains("london", "LondonBridge");
+            StringAssert.Contains("london", "london");
             StringAssert.StartsWith("Food", "FoodPanda");
             StringAssert.EndsWith("rangers", "Powerrangers");
             StringAssert.AreEqualIgnoringCase("DOG", "dog");
             StringAssert.IsMatch("[10-29]", "15");
-
+            StringAssert.DoesNotContain("abc", "defghijk");
+            StringAssert.DoesNotEndWith("abc", "abcdefgh");
+            StringAssert.DoesNotMatch("abc", "def");
+            string a1="abc";
+            string b1="abcd";
+            //StringAssert.ReferenceEquals(a1, b1); need to debug why it's failing
+            
             Assert.Contains(string.Empty, new List<object> {string.Empty},
                 "Assert.Contains is used to test whether an object is contained in a collection.");
 
@@ -186,7 +182,7 @@ namespace Nunit_CShaprp_CheatSheet
 
             #endregion
         }
-
+       
         /// <summary>
         /// Constraint based testing: Test cases with a fluent design pattern
         /// </summary>
@@ -303,6 +299,7 @@ namespace Nunit_CShaprp_CheatSheet
             // Throws Constraint
             // See also Property constrains for Message and Inner Exception
             // Throws.Exception allow AND, Throws.InstanceOf<> is short hand
+            #region exceptions
             Assert.That(() => new ArgumentException("Foo"),
                 Throws.Exception.InstanceOf<ArgumentException>().And.Message.EqualTo("Foo"));
 
@@ -318,7 +315,7 @@ namespace Nunit_CShaprp_CheatSheet
                 Throws.Exception.Message.EqualTo("Foo").And.InnerException.InstanceOf<ArgumentException>());
 
             Assert.That(() => new ArgumentException(), Throws.ArgumentException);
-
+#endregion
             #region Compound Constraints
 
             Assert.That(2, Is.Not.EqualTo(1));
@@ -354,6 +351,27 @@ namespace Nunit_CShaprp_CheatSheet
                 List.Map(new List<string> {"1", "22", "333"})
                     .Property("Length"), // Allows all elememts in a list to have a condition provided for them
                 Is.EqualTo(new List<int> {1, 2, 3}));
+
+            #endregion
+        }
+
+        [Test]
+        public void ExceptionAssertions()
+        {
+            #region Exceptions
+
+            //Using lambda functions with no parameters for ease of explanation/Implementation
+
+            Assert.Throws(typeof(ArgumentNullException), () => new ArgumentNullException());
+
+            Assert.Throws<ArgumentNullException>(() => new ArgumentNullException());
+
+            Assert.DoesNotThrow(() => { });
+
+            Assert.Catch(typeof(Exception), () => new ArgumentNullException());
+
+            // Similar as Throws but allows any derrived class of the thrown exception
+            Assert.Catch<Exception>(() => new ArgumentNullException());
 
             #endregion
         }
